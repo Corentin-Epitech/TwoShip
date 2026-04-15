7756318a3f1177c5a19571b130ad12a0df1a4af6
Corentin <corentin.courtine@epitech.eu>
2026-04-15 10:59:03 +0200

    Add: New unit test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const assert = require("assert")
 const { describe } = require("mocha")
 const Math = require("../src/math")
+const event = require("../src/events")
 
 describe('Math', () => {
     
@@ -31,5 +32,37 @@ describe('Math', () => {
             assert.strictEqual(Math.lerp(1.3,-7,2), -15.3)
         })
     })
+
+    describe('Clamp', () => {
+        it (' If range is 15-35 should return 15 for value 10 ', () => {
+            assert.strictEqual(Math.clamp(10,15,35), 15)
+        })
+        it (' If range is 15-35 should return 20 for value 20 ', () => {
+            assert.strictEqual(Math.clamp(20,15,35), 20)
+        })
+        it (' If range is 15-35 should return 35 for value 50 ', () => {
+            assert.strictEqual(Math.clamp(50,15,35), 35)
+        })
+    })
     
+    describe('RandFloat', () => {
+        it ('Should return true if number is between 5 and 10', () => {
+            const value = Math.randFloat(5, 10)
+            assert.strictEqual( value >= 5 && value <= 10 , true )
+        })
+        it ('Should return true if number is  between -10 and -5', () => {
+            const value = Math.randFloat(-10, -5)
+            assert.strictEqual( value <= -5 && value >= -10 , true )
+        })
+    })
+
+    
+})
+
+describe("Events", () => {
+    describe('Trigger', () => {
+        it ('Take a target , a type and an event, should return target', () => {
+            assert.strictEqual(event.trigger("target","type","event"), "target")
+        })
+    })
 })
